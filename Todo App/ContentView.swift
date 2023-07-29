f79e478866bc0c1d8b52c54c0e46103f508f2f97
YJ Soon <yjsoon@tinkertanker.com>
2023-07-29 11:19:20 +0800

    Added Todo struct and set it up on-screen

--- Todo App/ContentView.swift
+++ Todo App/ContentView.swift
@@ -8,14 +8,20 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var todos = [
+        Todo(title: "Buy 20kg of Nutella"),
+        Todo(title: "Call a courier for 20kg of Nutella"),
+        Todo(title: "Pack 20kg of Nutella in office"),
+        Todo(title: "Buy health insurance"),
+        Todo(title: "Eat 20kg of Nutella"),
+        Todo(title: "Regret life decisions")
+    ]
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        List(todos) { todo in
+            Text(todo.title)
         }
-        .padding()
     }
 }
 
---
+++ Todo App/Todo.swift
@@ -0,0 +1,16 @@
+//
+//  Todo.swift
+//  Todo App
+//
+//  Created by YJ Soon on 29/7/23.
+//
+
+import Foundation
+
+struct Todo: Identifiable {
+    
+    var id = UUID() // Universally Unique IDentifier
+    var title: String
+    var isCompleted = false
+    
+}
